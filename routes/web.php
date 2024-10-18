33ae9615296a14a5e3a0cb42e3bfc05899a4af8e
YuzakiOnly <aryamenk2007@gmail.com>
2024-10-18 15:29:33 +0700

    add:file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PersonalInformationController;
+use App\Models\PersonalInformation;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/personal-information', PersonalInformationController::class);
